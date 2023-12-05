ebdcbccf1ef3b0dbd46ba13cded7af4167c8e642
fluryh <fluryh@unc.edu>
2023-12-05 09:07:11 -0500

    Update NewHRSims.R
    Updated simulations

--- NewHRSims.R
+++ NewHRSims.R
@@ -4,7 +4,7 @@ library(evd)
 library(beepr)
 set.seed(5838)
 bm608 <- 2.96765
-bm5938<-4.05082
+bm37138<-4.05082
 bm1770297 <- 4.875154944523863
 p <- function(l,m){
   return(1-l^2/log(m))
@@ -21,31 +21,30 @@ grad <- function(l,X,Y){
   for(i in 1:d){
     x <- X[i]
     y <- Y[i]
-    sum <- sum -2*exp(-x)*dnorm(l + (y-x)/(2*l)) + exp(-x)*dnorm(l + (x-y)/(2*l))*(-1/2 - 1/(2*l^2)+(y-x)^2/(8*l^4) + pnorm(l + (x-y)/(2*l))*exp(-y)*(1-(y-x)/(2*l^2)) + pnorm(l + (y-x)/(2*l))*exp(-x)*(1-(x-y)/(2*l)))/(exp(-x-y)*pnorm(l + (x-y)/(2*l))*pnorm(l + (y-x)/(2*l)) + exp(-x)*dnorm(l + (y-x)/(2*l))/(2*l))
+    sum <- sum -2*exp(-x)*dnorm(l + (y-x)/(2*l)) + exp(-x)*dnorm(l + (y-x)/(2*l))*(-1/2 - 1/(2*l^2)+(y-x)^2/(8*l^4) + pnorm(l + (x-y)/(2*l))*exp(-y)*(1-(y-x)/(2*l^2)) + pnorm(l + (y-x)/(2*l))*exp(-x)*(1-(x-y)/(2*l^2)))/(exp(-x-y)*pnorm(l + (x-y)/(2*l))*pnorm(l + (y-x)/(2*l)) + exp(-x)*dnorm(l + (y-x)/(2*l))/(2*l))
   }
   return(sum)
 }
 
 log_liks <- rep(0,100)
 for(j in 1:100){
-  samps <- matrix(data = rep(0,2*565),ncol= 2) 
-  for(k in 1:565){
-    XY_scaled <- apply(mvrnorm(1770297, mu = c(-bm1770297^2,-bm1770297^2), Sigma = bm1770297^2*matrix(c(1, p2(1.25,bm1770297), p2(1.25,bm1770297), 1), ncol=2))
+  samps <- matrix(data = rep(0,2*269),ncol= 2) 
+  for(k in 1:269){
+    XY_scaled <- apply(mvrnorm(37138, mu = c(0,0), Sigma = matrix(c(1, p2(1.25,bm37138), p2(1.25,bm37138), 1), ncol=2))
                        ,2,max)
     samps[k,] <- XY_scaled
   }
-  print(apply(samps,2,var))
-  print(apply(samps,2,mean))
-  # samps <- samps 
-  # log_liks[j] <- sum/sqrt(565)
+  # samps[,1] <- (samps[,1] - mean(samps[,1]))* sqrt(var(loghr[,1])/var(samps[,1])) + mean(loghr[,1])
+  # samps[,2] <- (samps[,2] - mean(samps[,2]))* sqrt(var(loghr[,2])/var(samps[,2])) + mean(loghr[,2])
+  log_liks[j] <- grad(1.25, samps[,1], samps[,2])/sqrt(565)
 }
 mean(log_liks)
-hist(log)
+hist(log_liks)
 var(log_liks)
 
-loghr <- rbvhr(10000,1/1.332265)
-# XY <- exp(loghr)
-grad(1.25,loghr[,1],loghr[,2])/10000
+loghr <- rbvevd(100000,1/1.25, model = "hr")
+# loghr <- rmev(100000,d=2,sigma = matrix(data = c(0,1/1.25^2,1/1.25^2,0),nrow=2), model = "hr")
+grad(1.25,loghr[,1],loghr[,2])/100000
 beep()
 
 sum <- matrix(data = rep(0,2*1000),ncol= 2)
